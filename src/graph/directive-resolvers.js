b2bd14704f1efe62baca61b9f2ed7ef572ad053c
Josh Worden <josh@parameter1.com>
2024-05-09 12:18:10 -0500

    Ensure next is called

--- src/graph/directive-resolvers.js
+++ src/graph/directive-resolvers.js
@@ -3,6 +3,9 @@ module.exports = {
    *
    */
   readOnly(next, root, vars, { locked }) {
-    if (locked) throw new Error('This NativeX instance is inactive. Changes cannot be saved.');
+    return next().then((r) => {
+      if (locked) throw new Error('This NativeX instance is inactive. Changes cannot be saved.');
+      return r;
+    });
   },
 };
